c553a3fbd7fe31f67b6c182c6f25b52ca4da88f6
Jarsaq2077 <jarsaq@gmail.com>
2025-10-17 00:42:19 -0500

    musica invierno y paso de nivel

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
     public GameManager gameHandler;
     public AudioSource nextLap;
+    private int count = 0;
 
     private void OnTriggerEnter(Collider other)
     {        
         if (other.CompareTag("Player"))
         {
-            Debug.Log("The player character has touched the line.");
+            Debug.Log("The player character has touched the final line.");
             gameHandler.StopTimer();
             nextLap.volume = 0.5f;
             nextLap.Play();
+            if (count >= 2)
+            {
+                SceneManager.LoadScene("Nivel 2");
+            }
+            else
+            {
+                count++;
+            }
         }
     }
+    
 }
